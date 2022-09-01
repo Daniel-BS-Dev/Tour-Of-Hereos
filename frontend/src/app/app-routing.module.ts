8e77701238423556f2cfd47a498006f0578b9d80
Daniel-BS-Dev <danielbenedito355@gmail.com>
2022-09-01 13:38:41 -0300

    aplicando rota, lazy loading

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo:'home'},
+  {path:'home', loadChildren: () => import('./heroes/heroes.module').then((m) => m.HeroesModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
